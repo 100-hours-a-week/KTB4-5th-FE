22304759e5581fbf678568a5d80c78f8e649248f
iey704 <iey704@gachon.ac.kr>
2026-09-21 17:54:18 +0900

    refactor: 직접 등록 재료명 입력을 공통 피처로 교체

--- src/_pages/register-ingredient-manual/model/stock-key.ts
+++ src/_pages/register-ingredient-manual/model/stock-key.ts
@@ -1,6 +1,5 @@
 import type { IngredientStorageType } from "@/entities/ingredient";
-
-import { normalizeIngredientName } from "../lib/draft-input";
+import { normalizeIngredientName } from "@/features/ingredient-form";
 
 /**
  * 품목 종류 판정 키:
